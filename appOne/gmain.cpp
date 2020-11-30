fdc88e5e4a06037104334230fe8318ec663621f1
Coding-Ocean <eijo524@gmail.com>
2020-11-30 14:08:07 +0900

    プロジェクト ファイルを追加します。

---
+++ appOne/gmain.cpp
@@ -0,0 +1,10 @@
+#include"libOne.h"
+void gmain() {
+    window(400, 400);
+    while (notQuit) {
+        clear(240);
+        fill(0);
+        text("‚ ‚ñ‚È‚±‚Æ‚¢‚¢‚È", 0, 20);
+        text("‚Å‚«‚½‚ç‚¢‚¢‚È", 0, 40);
+    }
+}
