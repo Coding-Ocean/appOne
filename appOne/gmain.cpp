b288fe5f82e51fe9da24df66e9ecca15782de29b
Coding-Ocean <eijo524@gmail.com>
2020-12-08 09:29:31 +0900

    default

--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,10 +1,7 @@
 #include"libOne.h"
 void gmain() {
-    window(400, 400);
+    window(1000, 1000);
     while (notQuit) {
-        clear(240);
-        fill(0);
-        text("‚ ‚ñ‚È‚±‚Æ‚¢‚¢‚È", 0, 20);
-        text("‚Å‚«‚½‚ç‚¢‚¢‚È", 0, 40);
+        clear(128);
     }
 }
