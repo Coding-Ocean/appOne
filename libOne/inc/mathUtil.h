2e82819302815253cee28661498e79156f7de58f
Coding_Ocean <eijo524@gmail.com>
2022-06-25 15:19:17 +0900

    update libOne

--- libOne/inc/mathUtil.h
+++ libOne/inc/mathUtil.h
@@ -6,13 +6,27 @@ enum AXIS_MODE { DRAW, NODRAW };
 void axisMode(AXIS_MODE mode);
 void mathAxis(float maxScaleX=1.1f, float bright=0);
 void mathPoint(float x, float y);
-void mathCircle(float x, float y, float r);
+void mathCircle(float x, float y, float diameter);
 void mathRect(float x, float y, float w, float h, float angle = 0);
 void mathLine(float sx, float sy, float ex, float ey);
 void mathImage(int img, float x, float y, float angle = 0, float scale=1);
 void mathArrow(float sx, float sy, float ex, float ey, float size = 30.0f);
 void mathText(class let l, float x, float y);
 void mathArc(float startAngle, float centralAngle, float radius);
+//positionÇVECTORÇ≈éÛÇØéÊÇÈ,êFÇ∆ê¸ÇÃëæÇ≥Ç‡åàÇﬂÇÈÅB
+void mathPoint(const class VECTOR& p, const struct COLOR& c, float strokeW);
+void mathCircle(const class VECTOR& p, float diameter, 
+	const struct COLOR& fillC, const struct COLOR& c, float strokeW);
+void mathRect(const class VECTOR& p, float w, float h, float angle, 
+	enum RECT_MODE mode, const struct COLOR& fillC, const struct COLOR& c, float strokeW);
+void mathLine(const class VECTOR& sp, const class VECTOR& ep, const struct COLOR& c, float strokeW);
+void mathImage(int img, const class VECTOR& p, float angle = 0, float scale = 1);
+void mathArrow(const class VECTOR& sp, const class VECTOR& ep, 
+	const struct COLOR& c, float strokeW, float size = 30.0f);
+void mathText(class let l, const class VECTOR& p, enum TEXT_MODE mode, const struct COLOR& c, float size=30);
+void mathBracket(const class VECTOR& sp, const class VECTOR& ep, float r,
+	const struct COLOR& c, float strokeW);
+void mathArc(float startAngle, float centralAngle, float radius, const struct COLOR& c, float strokeW);
 extern float mathMouseX;
 extern float mathMouseY;
 
@@ -36,3 +50,4 @@ float Pow(float a, float b);
 float Abs(float a);
 float Round(float v);
 float Map(float v, float a1, float a2, float b1, float b2);
+float lerp(float a, float b, float f);
